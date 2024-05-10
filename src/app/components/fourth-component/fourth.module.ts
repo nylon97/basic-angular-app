f22d67d16564a3f58e6ef0e0575ff90c3715be7c
Nilo Iglesias Marina <panel.niglesias@iberia.es>
2024-05-10 16:58:58 +0200

    feat: add input, button and form field from material

--- src/app/components/fourth-component/fourth.module.ts
+++ src/app/components/fourth-component/fourth.module.ts
@@ -1,13 +1,19 @@
 import { NgModule } from "@angular/core";
 import { FourthComponent } from "./fourth.component";
 import { CommonModule } from "@angular/common";
+import {MatInputModule} from '@angular/material/input';
+import {MatTableModule} from '@angular/material/table';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
     declarations: [
         FourthComponent
     ],
     imports: [
-        CommonModule
+        CommonModule,
+        MatInputModule,
+        MatTableModule,
+        MatButtonModule
     ]
 })
 export class FourthModule {}
